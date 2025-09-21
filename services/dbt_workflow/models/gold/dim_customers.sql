eef34e95eeaf978c7a70013c825706f75e4537e0
Vinicius Torres <vini.guerra2@hotmail.com>
2025-09-21 03:07:43 -0300

    Fix: Ajustar erro de sintaxe na query

--- services/dbt_workflow/models/gold/dim_customers.sql
+++ services/dbt_workflow/models/gold/dim_customers.sql
@@ -4,15 +4,16 @@ SELECT
     ci.cst_key                         AS customer_number,
     ci.cst_firstname                   AS first_name,
     ci.cst_lastname                    AS last_name,
-    la.cntry                           AS country,
-    ci.cst_marital_status              AS marital_status,
+    la.cntry                            AS country,
+    ci.cst_marital_status               AS marital_status,
     CASE 
         WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr 
-        ELSE COALESCE(ca.gen, 'n/a')  			   
-    ca.bdate                           AS birthdate,
-    ci.cst_create_date                 AS create_date
-FROM {{ref('silver_crm_cust_info')}} ci
-LEFT JOIN {{ref('silver_erp_cust_az12')}} ca
+        ELSE COALESCE(ca.gen, 'n/a')
+    END                                AS gender,
+    ca.bdate                            AS birthdate,
+    ci.cst_create_date                  AS create_date
+FROM {{ ref('silver_crm_cust_info') }} ci
+LEFT JOIN {{ ref('silver_erp_cust_az12') }} ca
     ON ci.cst_key = ca.cid
-LEFT JOIN {{ref('silver_erp_loc_a101')}}  la
+LEFT JOIN {{ ref('silver_erp_loc_a101') }} la
     ON ci.cst_key = la.cid
